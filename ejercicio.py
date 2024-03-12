59fe98289f6060f80fbbd3b4d059837590178e47
Mateoochoagaleano <158522384+Mateoochoagaleano@users.noreply.github.com>
2024-03-12 17:57:06 -0500

    MATEO OCHOA GALEANO
    MATEO OCHOA GALEANO

---
+++ ejercicio.py
@@ -0,0 +1,7 @@
+def saludo(nombre, apellido):
+    print('Hola' (nombre) (apellido))
+
+nombre = input('Ingrese su nombre: ')
+apellido = input('Ingrese su apellido: ')
+
+print('HOLA', nombre, apellido)
